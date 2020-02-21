970e2ac84fe487b7b5e7aa07f8f6a0dbf004df5e
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-21 17:54:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,8 @@ class Song
   
   def self.new_from_filename(name)
     song = self.new 
-    song.name = (name.split("-"[1].chomp(".mp3")))
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    ar
   end 
   
   def self.create_from_filename 
